1beee1cfebbb04641a74305c0a568137598c47b8
Rune Jacobsen <slashrune@gmail.com>
2018-05-23 21:31:43 +0200

    43 added image to chat

--- src/Components/ChatBubble/ChatBubble.js
+++ src/Components/ChatBubble/ChatBubble.js
@@ -6,43 +6,42 @@ class ChatBubble extends Component {
   constructor() {
     super();
     this.state = {
-      isHidden : true
+      isHidden: true
     }
   }
 
   componentDidMount() {
 
-      setTimeout(() => {
-          this.fixThis();
-      }, this.props.wait);
+    setTimeout(() => {
+      this.fixThis();
+    }, this.props.wait);
   }
 
   fixThis = () => {
-    const {scrollToBottom} = this.props;
-    this.setState({isHidden : false});
+    const { scrollToBottom } = this.props;
+    this.setState({ isHidden: false });
 
     if (this.props.scrollToBottom)
       this.props.scrollToBottom();
   }
 
-  render() {
-    const { sender, first, last, children, chatbubbleContent } = this.props;
-    let container=`${sender}-container`;
-    let bubbleClass=`${sender}`;
-    if(first) bubbleClass+= ` ${sender}-first`;
-    if(last) bubbleClass+= ` ${sender}-last`;
+  componentw
 
-    return(
-       !this.state.isHidden &&
-        <div className={container}>
-          <div className={bubbleClass}>
+  render() {
+    const { sender, first, last, children, chatbubbleContent, image } = this.props;
+    let container = `${sender}-container`;
+    let bubbleClass = `${sender}`;
+    if (first) bubbleClass += ` ${sender}-first`;
+    if (last) bubbleClass += ` ${sender}-last`;
+
+    return (
+      !this.state.isHidden &&
+      <div className={container}>
+        {image && <img src={image} className='slider-image' />}
+        <div className={bubbleClass}>
           <p>{chatbubbleContent}</p>
-          {
-            children &&
-            children
-          }
-          </div>
         </div>
+      </div >
     );
   }
 }
