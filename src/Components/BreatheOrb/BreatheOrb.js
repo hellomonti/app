911bb6a7b636a608bdb526eb3b1ff8f5a6b8f5c3
LukasBysted <lukasbysted@gmail.com>
2018-05-25 12:13:21 +0200

    Added inhale exhale text

--- src/Components/BreatheOrb/BreatheOrb.js
+++ src/Components/BreatheOrb/BreatheOrb.js
@@ -4,10 +4,38 @@ import './BreatheOrb.css'
 
 class BreatheOrb extends Component {
 
+  state = {
+    orbtext: 'inhale'
+  }
+
+  componentDidMount(){
+    this.updateText();
+  }
+
+  updateText = () => {
+    //Sets the text to inhale
+    this.setState({
+      orbtext: 'inhale'
+    })
+
+    //Sets the text to exhale after 6500 ms
+    setTimeout(
+      () => this.setState({
+        orbtext: 'exhale'
+      }), 6000
+    )
+
+    setTimeout(
+      this.updateText
+    , 12000)
+  }
+
+
   render() {
     return(
       <div className='outerorb'>
         <div className='orb orb_1'>
+          {this.state.orbtext}
         </div>
       </div>
     )
